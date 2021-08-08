7d0b1d6bc2418f034027cbb00df0d4dfdb340f11
YeonjuSeo <tjduswn1219@gmail.com>
2021-08-09 00:55:29 +0900

    🎉 Start Project!

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+/** @jsxImportSource @emotion/react */
+import { jsx, css } from "@emotion/react";
+import styled from "@emotion/styled";
+
+const Home = () => {
+  return <Wrapper>hihi</Wrapper>;
+};
+
+export default Home;
+
+const Wrapper = styled.div`
+  background-color: #f0f1ec;
+`;
