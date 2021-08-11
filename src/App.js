bdcf77dad4ea762e173d19e42922f76a824460db
joonghyun <joongh@ewhain.net>
2021-08-11 23:54:59 +0900

    ⚗️

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 /** @jsxImportSource @emotion/react */
-import { jsx, css } from "@emotion/react";
-import styled from "@emotion/styled";
-import logo from "./logo.svg";
-import "./App.css";
+import { jsx, css } from '@emotion/react';
+import styled from '@emotion/styled';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <StyledText>
-          Edit <code>src/App.js</code> and save to reload.
-        </StyledText>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <div className="App">
+         <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <StyledText>
+               Edit <code>src/App.js</code> and save to reload.
+            </StyledText>
+            <a
+               className="App-lin"
+               href="https://reactjs.org"
+               target="_blank"
+               rel="noopener noreferrer"
+            >
+               Learn React
+            </a>
+         </header>
+      </div>
+   );
 }
 
 export default App;
 
 const StyledText = styled.div`
-  color: pink;
+   color: pink;
 `;
