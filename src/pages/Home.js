175d11168c7cd35ec370b8e7e858ba7248cd2bfb
YeonjuSeo <tjduswn1219@gmail.com>
2021-08-14 02:46:06 +0900

    ✨ 깃허브 로그인 구현

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,84 @@
 /** @jsxImportSource @emotion/react */
 import { jsx, css } from "@emotion/react";
 import styled from "@emotion/styled";
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import Login from "./Login";
+import NavBar from "../components/NavBar";
+
+const API_END_POINT = "3.36.73.136:8080";
 
 const Home = () => {
-  return <Wrapper>hihiplzplz</Wrapper>;
+  const history = useHistory();
+  const [giftList, setGiftList] = useState(null);
+  const [userInfo, setUserInfo] = useState();
+  const redirect = window.location.pathname;
+  async function getData() {
+    try {
+      //응답 성공
+      const response = await axios.get(`${API_END_POINT}/home`);
+      setGiftList(response);
+      console.log(response);
+    } catch (error) {
+      //응답 실패
+      console.error(error);
+    }
+  }
+  useEffect(() => {
+    (async () => {
+      setUserInfo(await getUserInfo());
+    })();
+  }, []);
+  async function getUserInfo() {
+    try {
+      const response = await fetch("/.auth/me");
+      const payload = await response.json();
+      const { clientPrincipal } = payload;
+      return clientPrincipal;
+    } catch (error) {
+      console.error("No profile could be found");
+      return undefined;
+    }
+  }
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <Wrapper>
+      <div>This is root page</div>
+      {!giftList && <div>선물을 등록해보세요!</div>}
+      {giftList &&
+        giftList.map((item) => {
+          <div
+            style={{
+              width: "200px",
+              height: "350px",
+              backgroundColor: "yellowgreen",
+            }}
+          >
+            기프티콘
+          </div>;
+        })}
+
+      {/* <Login /> */}
+      <NavBar />
+      <button
+        onClick={() => {
+          history.push("/save");
+        }}
+      >
+        등록하러가기
+      </button>
+      {/* <a href="/.auth/login/github">Login</a>
+      <a href="/.auth/logout">Log out</a> */}
+    </Wrapper>
+  );
 };
 
 export default Home;
 
 const Wrapper = styled.div`
   background-color: #f0f1ec;
+  display: block;
 `;
